0529a6373cdbf949f9a1859a3732e56432846fd4
Muhammad Shehu <magnanimus.shehu@gmail.com>
2017-04-14 20:45:59 +0000

    saving before making a branch. Added some comments probably

--- window.cpp
+++ window.cpp
@@ -6,20 +6,6 @@
 
 Window::Window() : gain(5), count(0)
 {
-	//knob = new QwtKnob;
-	// set up the gain knob
-	//knob->setValue(gain);
-
-	// use the Qt signals/slots framework to update the gain -
-	// every time the knob is moved, the setGain function will be called
-	//connect( knob, SIGNAL(valueChanged(double)), SLOT(setGain(double)) );
-
-	// set up the thermometer
-//	thermo = new QwtThermo; 
-//	thermo->setFillBrush( QBrush(Qt::red) );
-//	thermo->setRange(0, 20);
-//	thermo->show();
-
 	setup();
 
 	// set up the initial plot data
@@ -40,14 +26,15 @@ Window::Window() : gain(5), count(0)
 
 
 	// set up the layout - knob above thermometer
-	vLayout = new QVBoxLayout;
+//	vLayout = new QVBoxLayout;
 //	vLayout->addWidget(knob);
 //	vLayout->addWidget(thermo);
 
 	// plot to the left of knob and thermometer
 	hLayout = new QHBoxLayout;
-	hLayout->addLayout(vLayout);
+//	hLayout->addLayout(vLayout);
 	hLayout->addWidget(plot);
+	//only need one layout
 
 	setLayout(hLayout);
 
@@ -76,22 +63,18 @@ void Window::timerEvent( QTimerEvent * )
        // while(adcReader->hasSample())
 //	{
        // 	inVal =  (adcReader->getSample());
-
-		double inVal = gain * readData();
+		//currently it's not very complicated and readData is just a function without a class. 
+		double inVal = gain * readData(); //readData() is a function of adcReader2
 		++count;
 		//inVal = (1.25 + (1.25 * (inVal/32767)) - 0.75)/0.00125 - 502.5;
 
 	
         	// add the new input to the plot
-        	memmove( yData, yData+1, (plotDataSize-1) * sizeof(double) );
-        	yData[plotDataSize-1] = inVal;
-        	curve->setSamples(xData, yData, plotDataSize);
+        	memmove( yData, yData+1, (plotDataSize-1) * sizeof(double) );//shift ydata buffer?
+        	yData[plotDataSize-1] = inVal; //actual data value incoming
+        	curve->setSamples(xData, yData, plotDataSize);  
         	plot->replot();
 
-
-        	// set the thermometer value
-//        	thermo->setValue( inVal + 10 );
-	//}
 }
 
 
